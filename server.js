4e51bb20286d5c7bbd741267664afe8b9719cb4c
Neelesh <neeleshsingh767@gmail.com>
2020-12-24 18:36:23 +0530

    h

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const expressServer = app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
 const io = socketio(expressServer);
+io.configure(function () {
+    io.set("transports", ["xhr-polling"]);
+    io.set("polling duration", 10);
+});
 const helmet = require('helmet');
 app.use(helmet());
 
