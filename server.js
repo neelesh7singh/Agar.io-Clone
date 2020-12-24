967945fe95cde187fec37fead7b93c3d4b9de8d7
Neelesh <neeleshsingh767@gmail.com>
2020-12-24 17:16:15 +0530

    changed port for heroku

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 app.use(express.static(__dirname + '/public'));
 const socketio = require('socket.io');
-const expressServer = app.listen(8080, () => {
+const port = process.env.PORT || 80
+const expressServer = app.listen(port, () => {
     console.log('Server listening on port 8080');
 });
 const io = socketio(expressServer);
